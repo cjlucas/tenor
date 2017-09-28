f5f6e69c5cb89652deed5939b771aa5e43335738
Chris Lucas <chris@chrisjlucas.com>
2017-09-28 12:20:53 -0700

    Clean up Parser.parse_reader

--- lib/audiotag/parser.ex
+++ lib/audiotag/parser.ex
@@ -12,27 +12,8 @@ defmodule AudioTag.Parser do
   @parsers [AudioTag.MPEG, AudioTag.ID3v2, AudioTag.ID3v1] 
 
   def parse_reader(reader, acc) do
-    out = Enum.reduce_while(@parsers, reader, fn parser, reader ->
-      case parser.matches?(reader) do
-        {true, reader} ->
-          {:halt, {parser, reader}}
-        {false, reader} ->
-          {:cont, reader}
-      end
-    end)
-
-    {parser, reader} = case out do
-      {parser, reader} -> {parser, reader}
-      reader -> {nil, reader}
-    end
-
-    if is_nil(parser) do
-      #IO.puts "out of sync"
-      case AudioTag.FileReader.skip(reader, 1) do
-        {:ok, reader} -> parse_reader(reader, acc)
-        {:eof, reader} -> acc
-      end
-      else
+    case find_parser(reader) do
+      {parser, reader} ->
         case parser.parse(reader) do
           {:ok, stuff, reader} ->
             #IO.puts inspect stuff
@@ -40,6 +21,22 @@ defmodule AudioTag.Parser do
           {:error, _, reader} ->
             parse_reader(reader, acc)
         end
+      reader -> # no parser found
+        case AudioTag.FileReader.skip(reader, 1) do
+          {:ok, reader} -> parse_reader(reader, acc)
+          {:eof, reader} -> acc
+        end
     end
+  end
+
+  defp find_parser(reader) do
+    Enum.reduce_while(@parsers, reader, fn parser, reader ->
+      case parser.matches?(reader) do
+        {true, reader} ->
+          {:halt, {parser, reader}}
+        {false, reader} ->
+          {:cont, reader}
+      end
+    end)
   end
 end
