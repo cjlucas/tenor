daa25ae856e394b15a3eba962479cb57697bbb6d
Chris Lucas <chris@chrisjlucas.com>
2018-01-01 17:09:38 -0700

    Implement DB event handler in SearchService

--- search/service.go
+++ search/service.go
@@ -38,18 +38,28 @@ func buildSearchTrie(db *db.DB, coll *db.Collection, model interface{}) *Trie {
 		val := reflect.New(modelType).Elem()
 		db.ScanRows(rows, val.Addr().Interface())
 
-		id := val.FieldByIndex([]int{0, 0}).Interface().(string)
-		name := val.FieldByName("Name").Interface().(string)
-
-		for _, s := range strings.Split(name, " ") {
-			t.Add(strings.TrimSpace(s), id)
-
-		}
+		addModel(t, val.Addr().Interface())
 	}
 
 	rows.Close()
 
 	return t
+}
+
+func addModel(t *Trie, model interface{}) {
+	val := reflect.ValueOf(model)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	id := val.FieldByIndex([]int{0, 0}).Interface().(string)
+	name := val.FieldByName("Name").Interface().(string)
+
+	for _, s := range strings.Split(name, " ") {
+		t.Add(strings.TrimSpace(s), id)
+
+	}
 }
 
 func (s *Service) searchTrie(trie *Trie, query string) []string {
@@ -76,4 +86,19 @@ func (s *Service) SearchAlbums(query string) []string {
 
 func (s *Service) SearchTracks(query string) []string {
 	return s.searchTrie(s.tracksTrie, query)
+}
+
+func (s *Service) ArtistChanged(artist *db.Artist, eventType db.EventType) {
+	s.artistsTrie.DeleteValue(artist.ID)
+	addModel(s.artistsTrie, artist)
+}
+
+func (s *Service) AlbumChanged(album *db.Album, eventType db.EventType) {
+	s.albumsTrie.DeleteValue(album.ID)
+	addModel(s.albumsTrie, album)
+}
+
+func (s *Service) TrackChanged(track *db.Track, eventType db.EventType) {
+	s.tracksTrie.DeleteValue(track.ID)
+	addModel(s.tracksTrie, track)
 }
